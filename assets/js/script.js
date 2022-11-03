80a30c078d9ce4b242d2d37929c7233b18ce69be
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-03 15:11:47 -0300

    Add cards with js

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function createGame(player1, hour, player2){
+    return `
+    <li>
+        <img src="assets/imgs/bandeiras/${player1}.svg" alt="bendeira brasil">
+        <strong>${hour}</strong>
+        <img src="assets/imgs/bandeiras/${player2}.svg" alt="bandeira serbia">
+    </li>
+    `
+}
+
+function createCard(date, day, games){
+    return `
+    <div class="card">
+        <h2> ${date}<span>${day}</span></h2>
+        <ul>
+            ${games}
+        </ul>
+    </div>
+    `
+}
+
+document.querySelector('#app').innerHTML = `
+        <header>
+            <img src="./assets/imgs/logo.svg" alt="logo NLW Copa - Rocketseat">
+        </header>
+        <main class="cards">
+            ${createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia'))}
+            ${createCard('28/11', 'segunda', createGame('brazil', '13:00', 'switzerland') + createGame('portugal', '16:00', 'uruguay'))}
+            ${createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))}
+        </main>
+
+`
