6e7fa25338925506ea1754795aa89281929bf122
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-08 22:13:05 -0300

    Update and Readme

---
+++ README.md
@@ -0,0 +1,31 @@
+
+
+
+<h1 align="center"> NLW #10 Copa </h1>
+
+<p align="center">
+Evento exclusivo e gratuito, promovido pela Rocketseat para ensino de tecnologias WEB.
+</p>
+
+<p align="center">Link Projeto: <a href="https://arielemartins.github.io/NLW-Copa/">Calendário Copa</a></p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+![NLW-Copa-Desktop](https://user-images.githubusercontent.com/83427685/200710841-31377668-8f4c-4a4e-b24b-531399818834.png)
+
+#
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+
+## 💻 Projeto
+
+O Calendário da Copa é um projeto que mostra os jogos da Copa de 2022.
--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,21 @@ function createCard(date, day, games){
     `
 }
 
-document.querySelector('.cards').innerHTML = createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia') + createGame('portugal', '16:00', 'uruguay')) + createCard('28/11', 'segunda', createGame('brazil', '13:00', 'switzerland') ) + createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))
+document.querySelector('.cards').innerHTML = 
+    createCard('20/11', 'domingo', createGame('catar', '13:00', 'equador'))
+    + createCard('21/11', 'segunda', 
+        createGame('inglaterra', '10:00', 'iran')
+        + createGame('senegal', '13:00', 'holanda')
+        + createGame('estadosunidos', '16:00', 'gales')
+        )
+    + createCard('22/11', 'terça', createGame('argentina', '07:00', 'saudi arabia')
+        + createGame('denmark', '10:00', 'tunisia')
+        + createGame('mexico', '13:00', 'poland')
+        + createGame('france', '16:00', 'australia')
+        )
+    + createCard('23/11', 'quarta', createGame('switzerland', '07:00', 'cameroon')
+        + createGame('uruguay', '10:00', 'south korea')
+        + createGame('portugal', '13:00', 'ghana')
+        + createGame('brazil', '16:00', 'serbia')
+        )
+
